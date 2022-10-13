4a230829053ef61fe571d71bcb49c27f23119bdb
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-10-13 12:23:30 -0400

    styles list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,18 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-
-  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+  
 
 }
