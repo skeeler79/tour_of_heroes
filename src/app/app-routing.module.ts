4d200b5d4cb623f5442bec04eaf893057deda7c0
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-11-06 15:55:01 -0500

    adds dashboard and routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class AppRoutingModule { }
+
+
+//tour of heros replaces the code with this:
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
